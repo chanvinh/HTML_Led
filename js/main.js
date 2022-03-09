fae5c8c2313a5131d4d5236d2fd4c7b7d0ee1d13
ChanVinh <chanvinh41@gmail.com>
2022-03-09 13:53:58 +0700

    100%

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(".item_after").css({
+    "width": "15%",
+    "margin-left": "20px",
+    "margin-right": "30px"
+})
+
+
+$(".new_top_item_deital_products_body_products_sale.2").css({
+    "display": "none"
+})
+
+$(".new_top_item_deital_products_body_products_sale.4").css({
+    "display": "none"
+})
+
+// $("#menu").click(function() {
+//     $('.nav_right').toggleClass('nav_hide');
+// })
+
+$(".nav_left").click(function() {
+    $(".ul_nav").toggleClass("toggle_ul_nav");
+})
